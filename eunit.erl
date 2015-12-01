440374bf936a405033865fc353e08a57e16db380
Geoff Cant <nem@erlang.geek.nz>
2015-12-01 14:12:34 -0800

    mod_test: single module eunit suite template.

---
+++ eunit.erl
@@ -0,0 +1,23 @@
+-module('{{name}}_tests').
+
+-include_lib("eunit/include/eunit.hrl").
+
+-compile(export_all).
+
+%%====================================================================
+%% Tests
+%% function_test() -> for a single test
+%%     ?assert(boolean())
+%%     ?assertMatch(Pattern/Guard, Expression)
+%%     ?assertException(Class, Pattern, Expression)
+%%
+%% function_test_() -> % for test generators
+%%     [?_assert(boolean()),
+%%      ?_test(single test)].
+%%====================================================================
+
+
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
